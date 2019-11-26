20b96800b38719a01cb6cfabf7f20938d677020d
Dacey Nolan <daceyshirley@gmail.com>
2019-11-26 15:18:36 -0500

    Build text field for configure game and add styling

--- src/components/ConfigureGame/ConfigureGame.js
+++ src/components/ConfigureGame/ConfigureGame.js
@@ -5,8 +5,13 @@ import "./ConfigureGame.css";
 
 const ConfigureGame = () => {
   return (
-    <div className="edit-board">
+    <div className="configure-screen">
       <Header />
+      <div className="configure-screen__name-input-wrapper">
+        <input className="configure-screen__name-input" placeholder="Name" />
+        <input className="configure-screen__name-input" placeholder="Name" />
+        <input className="configure-screen__name-input" placeholder="Name" />
+      </div>
     </div>
   );
 };
