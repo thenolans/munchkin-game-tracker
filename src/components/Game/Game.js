2ddd6a609104a6e0772689054e0cd45292d4638f
Dacey Nolan <daceyshirley@gmail.com>
2019-11-27 13:38:07 -0500

    Create score input component to keep track of player score

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Button from "../Button";
+import ScoreInput from "../ScoreInput";
 import "./Game.css";
 
 const Game = () => {
@@ -9,16 +10,9 @@ const Game = () => {
     <div className="game">
       <div className="game__player-wrapper">
         Player Name
-        <div className="game__score-wrapper">
-          <button className="game__score-button game__score-button-decrement">
-            -
-          </button>
-          <div className="game__score">1</div>
-          <button className="game__score-button game__score-button-increment">
-            +
-          </button>
-        </div>
+        <ScoreInput />
       </div>
+
       <div className="game__button-wrapper">
         <Button as={Link} to="/results">
           End
---
+++ src/components/ScoreInput/ScoreInput.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import "./ScoreInput.css";
+
+const ScoreInput = () => {
+  const [playerScore, setPlayerScore] = useState(1);
+
+  const decrementScore = () => {
+    if (playerScore > 1) {
+      setPlayerScore(playerScore - 1);
+    }
+  };
+  const incrementScore = () => {
+    setPlayerScore(playerScore + 1);
+  };
+
+  return (
+    <div className="score-input">
+      <div className="score-input__wrapper">
+        <button
+          className="score-input__button score-input__button-decrement"
+          onClick={decrementScore}
+        >
+          -
+        </button>
+        <div className="score-input__score">{playerScore}</div>
+        <button
+          className="score-input__button score-input__button-increment"
+          onClick={incrementScore}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ScoreInput;
