42f44456688da693f40a341b33fb61fe3a362dfe
Dacey Nolan <daceyshirley@gmail.com>
2019-11-27 13:11:02 -0500

    Create imcrement and decrement button and style button

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,8 +7,18 @@ import "./Game.css";
 const Game = () => {
   return (
     <div className="game">
-      <div className="game__player">Player 1</div>
-
+      <div className="game__player-wrapper">
+        Player Name
+        <div className="game__score-wrapper">
+          <button className="game__score-button game__score-button-decrement">
+            -
+          </button>
+          <div className="game__score">1</div>
+          <button className="game__score-button game__score-button-increment">
+            +
+          </button>
+        </div>
+      </div>
       <div className="game__button-wrapper">
         <Button as={Link} to="/results">
           End
