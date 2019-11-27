99f50847c7318d85390e5c1534fdc53b69842801
Dacey Nolan <daceyshirley@gmail.com>
2019-11-27 15:54:12 -0500

    Create state from name input

--- src/components/NameInput/NameInput.js
+++ src/components/NameInput/NameInput.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./NameInput.css";
 
 const NameInput = () => {
+  const [playerName, setPlayerName] = useState("");
+
+  const changePlayerName = event => {
+    setPlayerName(event.target.value);
+  };
+
   return (
     <div className="name-input">
-      <input className="name-input__input" placeholder="Name" />
+      <input
+        className="name-input__input"
+        placeholder="Name"
+        value={playerName}
+        onChange={changePlayerName}
+      />
     </div>
   );
 };
