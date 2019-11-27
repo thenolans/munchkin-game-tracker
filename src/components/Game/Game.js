0bd5ed5e8017712e57146714723c282c98f4808f
Dacey Nolan <daceyshirley@gmail.com>
2019-11-27 10:47:56 -0500

    Create game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Button from "../Button";
+import "./Game.css";
 
 const Game = () => {
   return (
     <div className="game">
-      game score
+      <div className="game-player">Player 1</div>
+      <div className="game-player">Player 2</div>
+      <div className="game-player">Player 3</div>
+      <div className="game-player">Player 4</div>
       <Button as={Link} to="/results">
         End
       </Button>
