d1e8d3be9b233a124121c6de57c8344d32b81485
Dacey Nolan <daceyshirley@gmail.com>
2019-12-19 11:15:50 -0600

    Updates

---
+++ precache-manifest.a1111c9bec1493a83e32ca41b0eb5a7b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc60b73c0cbc1218e25f2eb6c723c501",
+    "url": "/munchkin-score-keeper/index.html"
+  },
+  {
+    "revision": "eb9c360ce33325444189",
+    "url": "/munchkin-score-keeper/static/css/main.3db09808.chunk.css"
+  },
+  {
+    "revision": "5860d16f52a6cfda0cbe",
+    "url": "/munchkin-score-keeper/static/js/2.bd2d409c.chunk.js"
+  },
+  {
+    "revision": "eb9c360ce33325444189",
+    "url": "/munchkin-score-keeper/static/js/main.be66594b.chunk.js"
+  },
+  {
+    "revision": "041c0d7f30f89bf2c964",
+    "url": "/munchkin-score-keeper/static/js/runtime-main.fe5699ed.js"
+  },
+  {
+    "revision": "cd3579e0fd766f206a1b0b334b4b8092",
+    "url": "/munchkin-score-keeper/static/media/dragon.cd3579e0.png"
+  },
+  {
+    "revision": "dd525d230964d7ad9584d1e88fdb024a",
+    "url": "/munchkin-score-keeper/static/media/king.dd525d23.jpg"
+  },
+  {
+    "revision": "e62fb4977453ce2803095a905c001cdd",
+    "url": "/munchkin-score-keeper/static/media/logo-transparent.e62fb497.png"
+  },
+  {
+    "revision": "b31c1c2e81cd152f7f2dac684fa030dc",
+    "url": "/munchkin-score-keeper/static/media/main-character.b31c1c2e.png"
+  },
+  {
+    "revision": "4d59c52aba585d41eca983d73c74db64",
+    "url": "/munchkin-score-keeper/static/media/man.4d59c52a.jpg"
+  }
+]);
